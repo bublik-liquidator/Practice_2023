83a0d0623be073636357b4f4b7f839514c7017c1
bublik-liquidator <iris_gonzales103@mail.ru>
2023-04-28 21:07:20 +0300

    Add env for DB, add init sql script

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require('dotenv').config();
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -7,21 +8,42 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
+// const errorHandler = (error, request, response, next) => {
+//     // Error handling middleware functionality
+//     console.log(`error ${error.message}`) // log the error
+//     const status = error.status || 400
+//     // send back an easily understandable error message to the caller
+//     response.status(status).send(error.message)
+// }
+// app.use(errorHandler)
+
 app.get("/", (request, response) => {
-  response.json({ info: "Node.js + Express + Postgres API=meetup.app" });
+    response.json({ info: "Node.js + Express + Postgres API=meetup.app" });
 });
 
-app.get("/meetup", db.getMeetup);
+app.get("/meetup",  db.getMeetup);
 app.get("/meetup/:id", db.getMeetupById);
 app.post("/meetup", db.createMeetup);
 app.put("/meetup/:id", db.updateMeetup);
 app.delete("/meetup/:id", db.deleteMeetup);
 
 app.listen(port, () => {
-  console.log(`Running on port ${port}.`);
+    console.log(`Running on port ${port}.`);
 });
+
+// process.on
+// (
+//     'uncaughtException',
+//     function (err)
+//     {
+//         console.log(err)
+//         var stack = err.stack;
+//         return null;
+//         //you can also notify the err/stack to support via email or other APIs
+//     }
+// );
--- validation/meetup.js
+++ validation/meetup.js
@@ -4,7 +4,7 @@ exports.meetupValidation = (data) => {
     name: Joi.string().min(2).max(255).required(),
     description: Joi.string().min(3).max(255).required(),
     tag: Joi.string().min(2).max(255).required(),
-    venue: Joi.string().min(2).max(255).required(),
+    place: Joi.string().min(2).max(255).required(),
     time: Joi.date().format("YYYY-MM-DD HH:mm:ssZZ"),
   });
   return schema.validate(data);
